435ba08568a969418c1b167eb9b7ab642766d49c
Scott Ammon <scott.o.ammon@gmail.com>
2018-05-29 20:59:38 -0700

    commented code and removed unecessary console.logs

--- js/app.js
+++ js/app.js
@@ -1,15 +1,16 @@
 function addToDo(e) {
   e.preventDefault();
 
+  // grab form data as array of objects
   var formData = $('form').serializeArray();
-  
-  console.log(formData);
-  console.log('length', formData[0]);
 
+  // only add to-do when text has been entered into the input field
   if(formData[0].value !== "") {
   
+    // create new elements that represent the list item
     $('.container').append("<div class='list-item'><p>" + formData[0].value + "</p><button class='delete'>&#10004</button></div>");
     
+    // reset input field to blank and focus on it
     $('input')[0].value = '';
     $('input')[0].focus();
 
@@ -24,12 +25,10 @@ function addToDo(e) {
 
 function deleteToDo(e) {
   e.preventDefault();
-  $(e.target).fadeOut('slow');
   $(e.target).parent().remove();
 }
 
 $(document).ready(function() {
-  
    // place focus on input field
    $('input')[0].focus();
 
